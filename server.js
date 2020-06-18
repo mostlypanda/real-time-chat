ce727d1c3196c78c30d09c09dd067da085cd26bd
mostlypanda <sarthakmittal1461@gmail.com>
2020-06-18 13:25:26 +0530

    main page uploaded

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const express=require('express');
 const app=express();
 
 //set static folder
-app.use(express.static());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-const PORT=3000 || p
+const PORT=3000 || process.env.PORT;
+
+app.listen(PORT,()=> console.log(`server running on port ${PORT}`));
